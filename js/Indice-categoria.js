de96e2fd42e05a6591bdb542b118ed5caed733c0
rfialkoski <ricardo.fialkoski@gmail.com>
2017-03-11 00:20:09 -0300

    add file

--- js/Indice-categoria.js
+++ js/Indice-categoria.js
@@ -1,1 +1,146 @@
-var postTitle=new Array();var postUrl=new Array();var postMp3=new Array();var postDate=new Array();var postLabels=new Array();var postBaru=new Array();var sortBy="titleasc";var tocLoaded=false;var numChars=250;var postFilter="";var numberfeed=0;function loadtoc(a){function b(){if("entry" in a.feed){var d=a.feed.entry.length;numberfeed=d;ii=0;for(var h=0;h<d;h++){var n=a.feed.entry[h];var e=n.title.$t;var m=n.published.$t.substring(0,10);var j;for(var g=0;g<n.link.length;g++){if(n.link[g].rel=="alternate"){j=n.link[g].href;break}}var o="";for(var g=0;g<n.link.length;g++){if(n.link[g].rel=="enclosure"){o=n.link[g].href;break}}var c="";if("category" in n){for(var g=0;g<n.category.length;g++){c=n.category[g].term;var f=c.lastIndexOf(";");if(f!=-1){c=c.substring(0,f)}postLabels[ii]=c;postTitle[ii]=e;postDate[ii]=m;postUrl[ii]=j;postMp3[ii]=o;if(h<10){postBaru[ii]=true}else{postBaru[ii]=false}ii=ii+1}}}}}b();sortBy="titleasc";sortPosts(sortBy);sortlabel();tocLoaded=true;displayToc2();document.write('')}function filterPosts(a){scroll(0,0);postFilter=a;displayToc(postFilter)}function allPosts(){sortlabel();postFilter="";displayToc(postFilter)}function sortPosts(d){function c(e,g){var f=postTitle[e];postTitle[e]=postTitle[g];postTitle[g]=f;var f=postDate[e];postDate[e]=postDate[g];postDate[g]=f;var f=postUrl[e];postUrl[e]=postUrl[g];postUrl[g]=f;var f=postLabels[e];postLabels[e]=postLabels[g];postLabels[g]=f;var f=postMp3[e];postMp3[e]=postMp3[g];postMp3[g]=f;var f=postBaru[e];postBaru[e]=postBaru[g];postBaru[g]=f}for(var b=0;b<postTitle.length-1;b++){for(var a=b+1;a<postTitle.length;a++){if(d=="titleasc"){if(postTitle[b]>postTitle[a]){c(b,a)}}if(d=="titledesc"){if(postTitle[b]<postTitle[a]){c(b,a)}}if(d=="dateoldest"){if(postDate[b]>postDate[a]){c(b,a)}}if(d=="datenewest"){if(postDate[b]<postDate[a]){c(b,a)}}if(d=="orderlabel"){if(postLabels[b]>postLabels[a]){c(b,a)}}}}}function sortlabel(){sortBy="orderlabel";sortPosts(sortBy);var a=0;var b=0;while(b<postTitle.length){temp1=postLabels[b];firsti=a;do{a=a+1}while(postLabels[a]==temp1);b=a;sortPosts2(firsti,a);if(b>postTitle.length){break}}}function sortPosts2(d,c){function e(f,h){var g=postTitle[f];postTitle[f]=postTitle[h];postTitle[h]=g;var g=postDate[f];postDate[f]=postDate[h];postDate[h]=g;var g=postUrl[f];postUrl[f]=postUrl[h];postUrl[h]=g;var g=postLabels[f];postLabels[f]=postLabels[h];postLabels[h]=g;var g=postMp3[f];postMp3[f]=postMp3[h];postMp3[h]=g;var g=postBaru[f];postBaru[f]=postBaru[h];postBaru[h]=g}for(var b=d;b<c-1;b++){for(var a=b+1;a<c;a++){if(postTitle[b]>postTitle[a]){e(b,a)}}}}function displayToc(a){var l=0;var h="";var e="Judul Artikel";var m="Klik untuk sortir berdasarkan judul";var d="Tanggal";var k="Klik untuk Sortir bedasarkan tanggal";var c="Kategori";var j="";if(sortBy=="titleasc"){m+=" (descending)";k+=" (newest first)"}if(sortBy=="titledesc"){m+=" (ascending)";k+=" (newest first)"}if(sortBy=="dateoldest"){m+=" (ascending)";k+=" (newest first)"}if(sortBy=="datenewest"){m+=" (ascending)";k+=" (oldest first)"}if(postFilter!=""){j="Klik untuk menampilkan semua"}h+="<table>";h+="<tr>";h+='<td class="toc-header-col1">';h+='<a href="javascript:toggleTitleSort();" title="'+m+'">'+e+"</a>";h+="</td>";h+='<td class="toc-header-col2">';h+='<a href="javascript:toggleDateSort();" title="'+k+'">'+d+"</a>";h+="</td>";h+='<td class="toc-header-col3">';h+='<a href="javascript:allPosts();" title="'+j+'">'+c+"</a>";h+="</td>";h+='<td class="toc-header-col4">';h+="Download MP3";h+="</td>";h+="</tr>";for(var g=0;g<postTitle.length;g++){if(a==""){h+='<tr><td class="toc-entry-col1"><a href="'+postUrl[g]+'">'+postTitle[g]+'</a></td><td class="toc-entry-col2">'+postDate[g]+'</td><td class="toc-entry-col3">'+postLabels[g]+'</td><td class="toc-entry-col4"><a href="'+postMp3[g]+'">Download</a></td></tr>';l++}else{z=postLabels[g].lastIndexOf(a);if(z!=-1){h+='<tr><td class="toc-entry-col1"><a href="'+postUrl[g]+'">'+postTitle[g]+'</a></td><td class="toc-entry-col2">'+postDate[g]+'</td><td class="toc-entry-col3">'+postLabels[g]+'</td><td class="toc-entry-col4"><a href="'+postMp3[g]+'">Download</a></td></tr>';l++}}}h+="</table>";if(l==postTitle.length){var f='<span class="toc-note">Menampilkan Semua '+postTitle.length+" Artikel<br/></span>"}else{var f='<span class="toc-note">Menampilkan '+l+" artikel dengan kategori '";f+=postFilter+"' dari "+postTitle.length+" Total Artikel<br/></span>"}var b=document.getElementById("toc");b.innerHTML=f+h}function displayToc2(){var a=0;var b=0;while(b<postTitle.length){temp1=postLabels[b];document.write('<p><a href="/search/label/'+temp1+'">'+temp1+"</a></p><ol>");firsti=a;do{document.write("<li>");document.write('<a href="'+postUrl[a]+'">'+postTitle[a]+"</a>");if(postBaru[a]==true){document.write(' - <strong><em><span style="color: rgb(255, 0, 0);">Novo!</span> </em></strong>')}document.write("</li>");a=a+1}while(postLabels[a]==temp1);b=a;document.write("</ol>");sortPosts2(firsti,a);if(b>postTitle.length){break}}}function toggleTitleSort(){if(sortBy=="titleasc"){sortBy="titledesc"}else{sortBy="titleasc"}sortPosts(sortBy);displayToc(postFilter)}function toggleDateSort(){if(sortBy=="datenewest"){sortBy="dateoldest"}else{sortBy="datenewest"}sortPosts(sortBy);displayToc(postFilter)}function showToc(){if(tocLoaded){displayToc(postFilter);var a=document.getElementById("toclink")}else{alert("Carregando...")}}function hideToc(){var a=document.getElementById("toc");a.innerHTML="";var b=document.getElementById("toclink");b.innerHTML='<a href="#" onclick="scroll(0,0); showToc(); Effect.toggle(\'toc-result\',\'blind\');">?? Menampilkan Daftar Isi</a>'}function looptemp2(){for(var a=0;a<numberfeed;a++){document.write("<br/>");document.write('Post Link		  : <a href="'+postUrl[a]+'">'+postTitle[a]+"</a><br/>");document.write('Download mp3  : <a href="'+postMp3[a]+'">'+postTitle[a]+"</a><br>");document.write("<br/>")}};
+/**
+ * Índice de postagens do blog by Elaine Gaspareto
+ * * URL: http://www.elainegaspareto.com
+ * TEMPLATES: &lt;div id="tabbed-toc"&gt;&lt;span class="loading"&gt;Loading...&lt;/span&gt;&lt;/div&gt;&lt;script type="text/javascript"&gt;var tabbedTOC = {blogUrl:"http://latitudu.blogspot.com", containerId: "tabbed-toc", activeTab: 1};&lt;/script&gt;&lt;script type="text/javascript" src="js/tabbed-toc.js"&gt;&lt;/script&gt;
+ */
+
+var tabbedTOC_defaults = {
+	blogUrl: "http://www.dte.web.id", // Blog URL
+	containerId: "tabbed-toc", // Container ID
+	activeTab: 1, // The default active tab index (default: the first tab)
+	showDates: false, // true to show the post date
+	showSummaries: false, // true to show the posts summaries
+	numChars: 200, // Number of summary chars
+	showThumbnails: false, // true to show the posts thumbnails (Not recommended)
+	thumbSize: 40, // Thumbnail size
+	noThumb: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAA3NCSVQICAjb4U/gAAAADElEQVQImWOor68HAAL+AX7vOF2TAAAAAElFTkSuQmCC", // No thumbnail URL
+	monthNames: [ // Array of month names
+		"Janeiro",
+		"Fevereiro",
+		"Março",
+		"Abril",
+		"Maio",
+		"Junho",
+		"Julho",
+		"Agosto",
+		"Setembro",
+		"Outubro",
+		"Novembro",
+		"Dezembro"
+	],
+	newTabLink: true, // Open link in new window?
+	maxResults: 99999, // Maximum posts result
+	preload: 0, // Load the feed after 0 seconds (option => time in milliseconds || "onload")
+	sortAlphabetically: true, // `false` to sort posts by date
+	showNew: false, // `false` to hide the "New!" mark in most recent posts, or define how many recent posts are to be marked
+	newText: " - <em style='color:red;'>Novo!</em>" // HTML for the "New!" text
+};
+
+for (var i in tabbedTOC_defaults) {
+	tabbedTOC_defaults[i] = (typeof(tabbedTOC[i]) !== undefined && typeof(tabbedTOC[i]) !== 'undefined') ? tabbedTOC[i] : tabbedTOC_defaults[i];
+}
+
+function clickTab(pos) {
+	var a = document.getElementById(tabbedTOC_defaults.containerId),
+		b = a.getElementsByTagName('ol'),
+		c = a.getElementsByTagName('ul')[0],
+		d = c.getElementsByTagName('a');
+	for (var t = 0; t < b.length; t++) {
+		b[t].style.display = "none";
+		b[parseInt(pos, 10)].style.display = "block";
+	}
+	for (var u = 0; u < d.length; u++) {
+		d[u].className = "";
+		d[parseInt(pos, 10)].className = "active-tab";
+	}
+}
+
+function showTabs(json) {
+
+	var total = parseInt(json.feed.openSearch$totalResults.$t,10),
+		c = tabbedTOC_defaults,
+		entry = json.feed.entry,
+		category = json.feed.category,
+		skeleton = "",
+		newPosts = [];
+
+	for (var g = 0; g < (c.showNew === true ? 5 : c.showNew); g++) {
+		if (g == entry.length) break;
+		entry[g].title.$t = entry[g].title.$t + (c.showNew !== false ? c.newText : '');
+	}
+
+	entry = c.sortAlphabetically ? entry.sort(function(a,b) {
+		return (a.title.$t.localeCompare(b.title.$t));
+	}) : entry;
+	category = c.sortAlphabetically ? category.sort(function(a,b) {
+		return (a.term.localeCompare(b.term));
+	}) : category;
+
+	// Build the tabs skeleton
+	skeleton = '<span class="divider-layer"></span><ul class="toc-tabs">';
+	for (var h = 0, cen = category.length; h < cen; h++) {
+		skeleton += '<li class="toc-tab-item-' + h + '"><a href="javascript:clickTab(' + h + ');">' + category[h].term + '</a></li>';
+	}
+	skeleton += '</ul>';
+
+	// Bulid the tabs contents skeleton
+	skeleton += '<div class="toc-content">';
+	for (var i = 0, cnt = category.length; i < cnt; i++) {
+		skeleton += '<ol class="panel" data-category="' + category[i].term + '"';
+		skeleton += (i != (c.activeTab-1)) ? ' style="display:none;"' : '';
+		skeleton += '>';
+		for (var j = 0; j < total; j++) {
+			if (j == entry.length) break;
+			var link, entries = entry[j],
+				pub = entries.published.$t, // Get the post date
+				month = c.monthNames, // Month array from the configuration
+				title = entries.title.$t, // Get the post title
+				summary = ("summary" in entries && c.showSummaries === true) ? entries.summary.$t.replace(/<br ?\/?>/g," ").replace(/<.*?>/g,"").replace(/[<>]/g,"").substring(0,c.numChars) + '&hellip;' : '', // Get the post summary
+				img = ("media$thumbnail" in entries && c.showThumbnails === true) ? '<img class="thumbnail" style="width:'+c.thumbSize+'px;height:'+c.thumbSize+'px;" alt="" src="' + entries.media$thumbnail.url.replace(/\/s72(\-c)?\//,"/s"+c.thumbSize+"-c/") + '"/>' : '<img class="thumbnail" style="width:'+c.thumbSize+'px;height:'+c.thumbSize+'px;" alt="" src="' + c.noThumb.replace(/\/s72(\-c)?\//,"/s"+c.thumbSize+"-c/") + '"/>', // Get the post thumbnail
+				cat = (entries.category) ? entries.category : [], // Post categories
+				date = (c.showDates) ? '<time datetime="' + pub + '" title="' + pub + '">' + pub.substring(8,10) + ' ' + month[parseInt(pub.substring(5,7),10)-1] + ' ' + pub.substring(0,4) + '</time>' : ''; // Formated published date
+				
+			for (var k = 0; k < entries.link.length; k++) {
+				if (entries.link[k].rel == 'alternate') {
+					link = entries.link[k].href; // Get the post URL
+					break;
+				}
+			}
+			for (var l = 0, check = cat.length; l < check; l++) {
+				var target = (c.newTabLink) ? ' target="_blank"' : ''; // Open link in new window?
+				// Write the list skeleton only if at least one of the post...
+				// ... has the same category term with one of the current categories term list
+				if (cat[l].term == category[i].term) {
+					skeleton += '<li title="' + cat[l].term + '"';
+					skeleton += (c.showSummaries) ? ' class="bold"' : '';
+					skeleton += '><a href="' + link + '"' + target + '>' + title + date + '</a>';
+					skeleton += (c.showSummaries) ? '<span class="summary">' + img + summary + '<span style="display:block;clear:both;"></span></span>' : '';
+					skeleton += '</li>';
+				}
+			}
+		}
+		skeleton += '</ol>';
+	}
+
+	skeleton += '</div>';
+	skeleton += '<div style="clear:both;"></div>';
+	document.getElementById(c.containerId).innerHTML = skeleton;
+	clickTab(c.activeTab-1);
+
+}
+
+(function() {
+	var h = document.getElementsByTagName('head')[0],
+		s = document.createElement('script');
+		s.type = 'text/javascript';
+		s.src = tabbedTOC_defaults.blogUrl + '/feeds/posts/summary?alt=json-in-script&max-results=' + tabbedTOC_defaults.maxResults + '&orderby=published&callback=showTabs';
+	if (tabbedTOC_defaults.preload !== "onload") {
+		setTimeout(function() {
+			h.appendChild(s);
+		}, tabbedTOC_defaults.preload);
+	} else {
+		window.onload = function() {
+			h.appendChild(s);
+		};
+	}
+})();
